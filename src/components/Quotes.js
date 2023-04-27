6d1fea6fc9a7691c44ff154f5f5e51dde670e119
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-04-27 17:49:51 -0300

    Add error and styles

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,22 +1,54 @@
 import { useState, useEffect } from 'react';
 
 function Quotes() {
+  // Initialize state for quotes
   const [quotes, setQuotes] = useState([]);
+  // Initialize state for loading status
+  const [isLoading, setIsLoading] = useState(true);
+  // Initialize state for error status
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const category = 'success';
     const fetchData = async () => {
-      const res = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-        headers: {
-          'X-Api-Key': '4ClfOE/97HHOf/lDFSpIJA==JqOaqEP6vNaKD1HF',
-        },
-      });
-      const data = await res.json();
-      setQuotes(data);
+      try {
+        // Set loading state to true while fetching data
+        setIsLoading(true);
+        const res = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+          headers: {
+            'X-Api-Key': '4ClfOE/97HHOf/lDFSpIJA==JqOaqEP6vNaKD1HF',
+          },
+        });
+        const data = await res.json();
+        // Update quotes state with fetched data
+        setQuotes(data);
+      } catch (error) {
+        // Set error state if fetch fails
+        setError(error);
+      } finally {
+        // Set loading state to false once fetch completes (with or without errors)
+        setIsLoading(false);
+      }
     };
     fetchData();
   }, []);
 
+  // Show loading state while data is being fetched
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  // Show error state if fetch fails
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error.message}
+      </div>
+    );
+  }
+
+  // Show each quote returned by the API in a div with class "quotes"
   return (
     <ul>
       {quotes.map((quote) => (
